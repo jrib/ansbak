61cd6fb6d7daa99b079170492508b990b311cfa9
Jason A. Ribeiro <jason@jrib.net>
2013-11-21 22:06:01 -0500

    Commit actual code!

---
+++ ansbak.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+"""
+format ansible output for humans and more
+"""
+
+
+import re
+import sys
+import collections
+
+
+HEADER = re.compile('(?P<hostname>\S*) \| (?P<return_msg>\S*) \| rc=(?P<return_code>\S*) >>')
+
+
+def main():
+    results = collections.defaultdict(list)
+
+    host = ''
+    out = ''
+    return_msg = ''
+    rc = ''
+
+    for line in sys.stdin:
+        m = HEADER.match(line)
+        if m:
+            # new host
+            # save output of previous host if any
+            if host:
+                key = (return_msg, rc, out)
+                value = host
+                results[key].append(value)
+            # now set these values for the new host
+            (host, return_msg, rc) = m.groups()
+            out = ''
+        else:
+            out += line
+
+    # save output of last host
+    if host:
+        key = (return_msg, rc, out)
+        value = host
+        results[key].append(value)
+
+    for (return_msg, rc, out), hostgroup in results.iteritems():
+        print '{hostgroup} | {return_msg} | rc={rc} >>'.format(
+            hostgroup=hostgroup,
+            return_msg=return_msg,
+            rc=rc)
+        print out
+
+
+if __name__ == '__main__':
+    main()
