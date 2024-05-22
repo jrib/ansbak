464fa1404a7b462d98dbb11bcc740f0010e665e8
Jason Ribeiro <jason@jrib.net>
2024-05-21 20:55:47 -0400

    Reorganize a bit

--- ansbak.py
+++ ansbak.py
@@ -3,13 +3,24 @@
 format ansible output for humans and more
 """
 
-
 import re
 import sys
 import collections
 
 
 HEADER = re.compile(r'(?P<hostname>\S*) \| (?P<return_msg>\S*) (?P<return_code>\| rc=\S* >>|=> {)')
+
+
+def sort_hostgroups(k):
+    (key, hostgroups) = k
+    return hostgroups
+
+
+def sort_return_msg(k):
+    (key, hostgroups) = k
+    (return_msg, rc, out) = key
+    return return_msg
+
 
 def main(lines):
     results = collections.defaultdict(list)
@@ -18,15 +29,6 @@ def main(lines):
     out = ''
     return_msg = ''
     rc = ''
-
-    def sort_hostgroups(k):
-        (key, hostgroups) = k
-        return hostgroups
-
-    def sort_return_msg(k):
-        (key, hostgroups) = k
-        (return_msg, rc, out) = key
-        return return_msg
 
     for line in lines:
         m = HEADER.match(line)
@@ -69,14 +71,12 @@ def main(lines):
     # Sort each grouped output based on reverse status first (so
     # "UNREACHABLE!" comes out before "CHANGED"), then (already sorted
     # and comma-separated) hostgroup, second
-    for i, ((return_msg, rc, out), hostgroup) in enumerate(sorted(sorted(results.items(), key=sort_hostgroups), key=sort_return_msg, reverse=True)):
+    sorted_results = sorted(sorted(results.items(), key=sort_hostgroups), key=sort_return_msg, reverse=True)
+    for i, ((return_msg, rc, out), hostgroup) in enumerate(sorted_results):
         if i != 0:
             print('--------------------')
 
-        print('{hostgroup} | {return_msg} {rc}'.format(
-            hostgroup=hostgroup,
-            return_msg=return_msg,
-            rc=rc))
+        print(f'{hostgroup} | {return_msg} {rc}')
         print(out)
 
 
